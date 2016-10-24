daf64328870dcdcf1901e03c1865e48e24e9a3cf
Your Name <hellobb@gmail.com>
2016-10-24 15:01:40 +0800

    差重生

---
+++ raytest/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown ()
+	{
+		//Debug.Log("12312");
+
+		transform.position = new Vector3(8,0.92f,0);
+		print(transform.position.x);
+	}
+}
