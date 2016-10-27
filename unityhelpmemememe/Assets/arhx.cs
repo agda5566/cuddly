8768aed7c4f38e4473da478c8afbae407831c7ea
張揚 <241092034@nutc.edu.tw>
2016-10-27 17:43:00 +0800

    time

---
+++ unityhelpmemememe/Assets/arhx.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arhx : MonoBehaviour {
+	//https://home.gamer.com.tw/creationDetail.php?sn=2275726
+	//https://www.ptt.cc/bbs/GameDesign/M.1442984491.A.6CD.html
+	int timer_i=0;
+	bool start_Timer3=true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ unityhelpmemememe/Assets/timetset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timetset : MonoBehaviour {
+
+	public GameObject projectile;
+	public float fireRate = 10.5F;
+	private float nextFire = 5.0F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			GameObject clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+		}
+	}
+}
