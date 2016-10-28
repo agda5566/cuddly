7ae2c97405d3253409dbb1dc4a8a971b84fbc9c9
張揚 <241092034@nutc.edu.tw>
2016-10-28 11:43:10 +0800

    計時器不能歸零GGG

--- unityhelpmemememe/Assets/firetest.cs
+++ unityhelpmemememe/Assets/firetest.cs
@@ -6,9 +6,10 @@ public class firetest : MonoBehaviour {
 	public GameObject projectile;
 	public float fireDelta = 0.5F;
 
-	private float nextFire = 0.5F;
-	private GameObject newProjectile;
-	private float myTime = 0.0F;
+	private float nextFire = 5.0F;
+	//private GameObject newProjectile;
+	float myTime = 5.0F;
+	int hetimer_i;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,16 +17,23 @@ public class firetest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		myTime = myTime + Time.deltaTime;
-
-		if (Input.GetButton("Fire1") && myTime > nextFire) {
+		myTime += Time.deltaTime;
+		//hetimer_i = (int)myTime;
+		//Debug.Log (hetimer_i);
+		if (Input.GetMouseButton(0) && myTime > nextFire) {
 			nextFire = myTime + fireDelta;
-			newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-
+			//newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+			print("我被按下了");
 			// create code here that animates the newProjectile        
 
+
+	}
+		if (Input.GetMouseButtonUp(0)){
+			Debug.Log("Pressed left click.");
+			//myTime += Time.deltaTime;
+			//myTime = Time.deltaTime%5;
 			nextFire = nextFire - myTime;
 			myTime = 0.0F;
-	}
+		}
 }
 }
